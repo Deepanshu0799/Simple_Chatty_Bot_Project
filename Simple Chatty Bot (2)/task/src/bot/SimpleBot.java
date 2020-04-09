84860c2b4169557daab88299a43500c3e28f50a5
DEEPANSHU <deepanshu.gupta_cs18@gla.ac.in>
2020-04-09 11:14:25 +0600

    First Problem Solved.

---
+++ Simple Chatty Bot (2)/task/src/bot/SimpleBot.java
@@ -0,0 +1,11 @@
+package bot;
+
+import java.util.Scanner;
+
+public class SimpleBot {
+    public static void main(String[] args) {
+
+        System.out.println ( "Hello! My name is Aid." );
+        System.out.println ( "I was created in 2018." );
+    }
+}
